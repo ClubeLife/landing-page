da6c80a1a9acfea87ceb990b0f7b400994f4b9a6
Henrique Nogueira <hrqnogueira@gmail.com>
2018-03-21 23:16:59 -0300

    Associate model

--- landing/core/admin.py
+++ landing/core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Associate
+
+
+@admin.register(Associate)
+class AssociateModelAdmin(admin.ModelAdmin):
+    list_display = 'user_email', 'first_name', 'last_name', 'tax_id', 'phone', 'created_at'
+    search_fields = 'first_name', 'last_name', 'tax_id'
+    date_hierarchy = 'created_at'
+
+    def user_email(self, obj):
+        return obj.user.email
--- landing/core/models.py
+++ landing/core/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Associate(models.Model):
     user = models.OneToOneField(User, related_name='associate', on_delete=models.CASCADE)
-    influenced_by = models.ForeignKey('Associate', related_name='influenced', on_delete=models.CASCADE)
+    influenced_by = models.ForeignKey('Associate', related_name='influenced', blank=True, null=True, on_delete=models.CASCADE)
     first_name = models.CharField('primeiro nome', max_length=128)
     last_name = models.CharField('sobrenome', max_length=128)
     tax_id = models.CharField('cpf', max_length=14)
