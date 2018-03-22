af421173eb001207a8d7a4b6bc1ff9a0ef69579a
Henrique Nogueira <hrqnogueira@gmail.com>
2018-03-21 23:07:03 -0300

    Associate model

--- landing/core/models.py
+++ landing/core/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Associate(models.Model):
+    user = models.OneToOneField(User, related_name='associate', on_delete=models.CASCADE)
+    influenced_by = models.ForeignKey('Associate', related_name='influenced', on_delete=models.CASCADE)
+    first_name = models.CharField('primeiro nome', max_length=128)
+    last_name = models.CharField('sobrenome', max_length=128)
+    tax_id = models.CharField('cpf', max_length=14)
+    phone = models.CharField('telefone', max_length=32)
+
+    created_at = models.DateTimeField('criado em', auto_now_add=True)
+    modified_at = models.DateTimeField('modificado em', auto_now=True)
