44b45c46588bebc20a839ab79cdf0d6eefad8636
Henrique Nogueira <hrqnogueira@gmail.com>
2018-03-24 22:55:55 -0300

    updated associates page

--- landing/core/admin.py
+++ landing/core/admin.py
@@ -5,9 +5,12 @@ from .models import Associate
 
 @admin.register(Associate)
 class AssociateModelAdmin(admin.ModelAdmin):
-    list_display = 'user_email', 'tax_id', 'phone', 'created_at'
+    list_display = 'user_email', 'influenced_by', 'tax_id', 'phone', 'created_at'
     search_fields = 'tax_id',
     date_hierarchy = 'created_at'
 
     def user_email(self, obj):
         return obj.user.email
+
+    def influenced_by(self, obj):
+        return obj.influence_by.user.email
