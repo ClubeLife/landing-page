12986affa10afb99726c766684d2bf014e419af4
Henrique Nogueira <hrqnogueira@gmail.com>
2018-03-24 21:38:57 -0300

    limpando essa zona

--- landing/core/models.py
+++ landing/core/models.py
@@ -3,8 +3,18 @@ from django.db import models
 
 
 class Associate(models.Model):
-    user = models.OneToOneField(User, related_name='associate', on_delete=models.CASCADE)
-    influenced_by = models.ForeignKey('Associate', related_name='influenced', blank=True, null=True, on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        User,
+        related_name='associate',
+        on_delete=models.CASCADE
+    )
+    influenced_by = models.ForeignKey(
+        'Associate',
+        related_name='influenced',
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE
+    )
     date_of_birth = models.DateField('data de nascimento')
     tax_id = models.CharField('cpf', max_length=14)
     phone = models.CharField('telefone', max_length=32)
